e4b03b8315d4f499f89b89161352311dd435d5a6
bezdatiuzer <bezdatiuzer@Stevee>
2015-10-26 21:19:03 +0500

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties

--- src/java/controllers/MessageController.java
+++ src/java/controllers/MessageController.java
@@ -6,6 +6,7 @@
 package controllers;
 
 import controllers.parent.WebController;
+import entities.User;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import service.AdService;
 import service.MessageService;
 
 /**
@@ -26,17 +28,22 @@ public class MessageController extends WebController {
     @Autowired
     MessageService msgService;
     
+    @Autowired
+    AdService adService;
+    
     @RequestMapping("/send")
     public String administrating (Map<String, Object> model,
-            @RequestParam(value = "senderId", required = false) Long senderId,
-            @RequestParam(value = "receiverId", required = false) Long receiverId,
             @RequestParam(value = "adId", required = false) Long adId,
-            @RequestParam(value = "text", required = false) String text,
+            @RequestParam(value = "message", required = false) String text,
             @RequestParam(value = "wish", required = false) String wish,
             HttpServletRequest request,RedirectAttributes ras) throws Exception {
         
-            
-           
+            User u = authManager.getCurrentUser();
+            if(u!=null){
+                msgService.create(u, text, adService.getAd(adId));
+                ras.addFlashAttribute(ERRORS_LIST_NAME, msgService.getErrors());
+            }
+            ras.addFlashAttribute("wish", wish);
         return "/Main/";
     }
     
